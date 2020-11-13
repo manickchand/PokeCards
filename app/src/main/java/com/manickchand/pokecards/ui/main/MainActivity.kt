c285cd7cc341363b168f7ede1115d737b4360e9d
leon.gruporbs <tc.leonjunior@gruporbs.com.br>
2020-11-13 16:58:05 -0300

    add filter and header

--- app/src/main/java/com/manickchand/pokecards/ui/main/MainActivity.kt
+++ app/src/main/java/com/manickchand/pokecards/ui/main/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.manickchand.pokecards.ui.main
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import com.manickchand.pokecards.R
 import com.manickchand.pokecards.model.PokemonModel
 import com.manickchand.pokecards.repository.PokeCardsRemoteSource
@@ -10,6 +12,7 @@ import com.manickchand.pokecards.repository.RetrofitInit.getClient
 import com.manickchand.pokecards.ui.detail.DetailDialogFragment
 import com.manickchand.pokecards.utils.showToast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.header.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,18 +22,37 @@ class MainActivity : AppCompatActivity(), MainListener {
 
     private val mRetrofit: Retrofit = getClient()
     private val pokeCardsRemoteSource: PokeCardsRemoteSource = this.mRetrofit.create(PokeCardsRemoteSource::class.java)
+    private val pokemonsList = ArrayList<PokemonModel>()
+    private val allPokemonsList = ArrayList<PokemonModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupRefresh()
+        setupRecycler()
         getPokemons()
 
+        et_filter.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                filterList(s.toString())
+            }
+        })
+
+    }
+
+    private fun setupRefresh(){
+        load.setOnRefreshListener{
+            getPokemons()
+        }
     }
 
     private fun getPokemons() {
 
-        load.isVisible = true
+        load.isRefreshing = true
 
         var call = this.pokeCardsRemoteSource.getPokemons()
 
@@ -39,26 +61,46 @@ class MainActivity : AppCompatActivity(), MainListener {
                 call: Call<List<PokemonModel>>?,
                 response: Response<List<PokemonModel>>?
             ) {
-                load.isVisible = false
-                setupRecycler(response?.body() ?: emptyList())
+                load.isRefreshing = false
+                addItems(response?.body() ?: emptyList())
+                showItems(allPokemonsList)
             }
 
             override fun onFailure(call: Call<List<PokemonModel>>?, t: Throwable?) {
-                load.isVisible = false
+                load.isRefreshing = false
                 showToast("Erro ao carregar pokemons")
             }
         })
     }
 
-    private fun setupRecycler(items: List<PokemonModel>) {
+    private fun setupRecycler() {
         with(recycler) {
             setHasFixedSize(true)
-            adapter = MainAdapter(items, this@MainActivity)
+            adapter = MainAdapter(pokemonsList, this@MainActivity)
         }
+    }
+
+    private fun showItems(pokemons: List<PokemonModel>){
+        pokemonsList.clear()
+        pokemonsList.addAll(pokemons)
+        recycler.adapter?.notifyDataSetChanged()
+    }
+
+    private fun addItems(pokemons: List<PokemonModel>){
+        et_filter.text.clear()
+        allPokemonsList.clear()
+        allPokemonsList.addAll(pokemons)
     }
 
     override fun clickPokemon(pokemonModel: PokemonModel) {
         DetailDialogFragment.newInstance(pokemonModel).show(supportFragmentManager, "Card Pokemon")
+    }
+
+    private fun filterList(filterStr: String?){
+     val pokemonsFiltered = filterStr?.run {
+        allPokemonsList.filter { it.name.toLowerCase().contains(filterStr.toLowerCase()) }
+     } ?: allPokemonsList
+        showItems(pokemonsFiltered)
     }
 
 }
--- app/src/main/java/com/manickchand/pokecards/utils/PokeCardsExtensions.kt
+++ app/src/main/java/com/manickchand/pokecards/utils/PokeCardsExtensions.kt
@@ -3,7 +3,6 @@ package com.manickchand.pokecards.utils
 import android.content.Context
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
@@ -17,4 +16,4 @@ fun ImageView.loadGlideImage(
         .into(this)
 }
 
-fun AppCompatActivity.showToast(msg:String) = Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+fun Context.showToast(msg:String) = Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
